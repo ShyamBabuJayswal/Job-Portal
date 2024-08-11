0b7c07c2697ae45e76a0491f28a01d71e0943884
NIhal <jaisnj541@gmail.com>
2024-08-11 18:41:47 +0530

    company model completed successfully

---
+++ backend/models/company.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const companySchema=new mongoose.Schema(
+    {
+       name:{
+        type:String,
+        required:true,
+       },
+       description:{
+        type:String,
+       
+       },
+       website:{
+        type:String,
+       
+       },
+       location:{
+        type:String,
+        required:true,
+       },
+       logo:{
+        type:String, //url to company logo
+
+       },
+       userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+       }
+
+    },
+    {timestamps:true}
+     )
+
+
+exports const  Company = mongoose.model("Company",companySchema);   
