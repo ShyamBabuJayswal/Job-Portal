91a8de1e98504820f76e10964b68ea0e44121f56
NIhal <jaisnj541@gmail.com>
2024-08-11 08:51:57 +0530

    Database created Succesfully

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app = express();
 // app.get("/Home",(req,res) =>{
@@ -23,9 +26,10 @@ app.use(cors(corsOption));
 
 
 
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server running in port ${PORT}`);
 })
 
---
+++ backend/utils/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB  = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("mongodb connected succesfully")
+    } catch (error) {
+      console.log(error);
+         
+    }
+}
+
+export default connectDB;
