70a640458ada2c72c225088f8eafa0cd5ff66d7f
NIhal <jaisnj541@gmail.com>
2024-08-12 13:40:05 +0530

    Regiester register part complete in user-controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,6 @@
+
+import {User} from "../models/user.model.js"
+import bcrypt from "bcryptjs";
 export const register  = async(req,res) => {
     try {
        
@@ -8,10 +11,29 @@ export const register  = async(req,res) => {
             message:"Something is missing",
             success:false
            })
+           const user = await UserActivation.findOne({email})
+           if(user){
+            return res.status(400).json({
+                message:"User already exists with this email",
+                success:true,
+            })
+           }
         }
+        const hashedPassowrd = await bcrypt.hash(password,10);
+
+        await UserActivation.create({
+            fullname,
+            email,
+            password:hashedPassowrd,
+            role,
+            
+        })
+
     } catch (error) {
         
     }
 
 }
+
+
     
