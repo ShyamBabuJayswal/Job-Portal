4d6639f00bcbd3650d6c143a4525a85d1b38dad6
NIhal <jaisnj541@gmail.com>
2024-08-12 14:04:12 +0530

    logout function completed successfully

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -98,4 +98,15 @@ export const login =  async(req,res)=>{
         console.log(error);
     }
 }
+
+export const logout = async(req,res)=>{
+    try {
+       return res.status(200).cookie("token","",{maxAge:0}).json({
+        message:"Logged out successfully",
+        success:true,
+       })  
+    } catch (error) {
+        console.log(error);
+    }
+}
     
