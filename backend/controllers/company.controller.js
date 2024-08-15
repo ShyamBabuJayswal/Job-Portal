6fa3b553d5059060d596d5810ed097f7222fe1a0
NIhal <jaisnj541@gmail.com>
2024-08-15 11:31:18 +0530

    register company and get company fn complete

---
+++ backend/controllers/company.controller.js
@@ -0,0 +1,52 @@
+
+import {Company} from "../models/company.model.js"
+export const registerCompany =async(req,res)=>{
+     try {
+        const {companyName} = req.body;
+        if(!companyName){
+            return res.status(400).json({
+                message:"Company name is required",
+                success:false
+            });
+        }
+        let company = await Company.findOne({name:companyName});
+        if(company){
+            return res.status(400).json({
+              message:"You can't register same company",
+              sucess:false,
+            });
+            
+        }
+        company =await Company.create({
+            name:company.name,
+            userId:req.id,
+
+      } );
+    
+      return res.status(201).json({
+        message:"Company registerd succesfully",
+        sucess:true,
+      })
+
+
+        
+     } catch (error) {
+       console.log(error); 
+     }
+    }
+
+
+export const getCompany = async(req,res)=>{
+    try {
+        const userId = req.id;//logged in user id
+        const companies=await Company.find({userId});
+        if(!companies){
+            return res.status(404).json({
+                message:"Company not found",
+                success:false
+            })
+        }
+    } catch (error) {
+      console.log(error);  
+    }
+}
--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -5,6 +5,7 @@ const companySchema=new mongoose.Schema(
        name:{
         type:String,
         required:true,
+        unique:true,
        },
        description:{
         type:String,
