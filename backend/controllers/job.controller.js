b776176370982ea7b3a7f122e2220f19399752c3
NIhal <jaisnj541@gmail.com>
2024-08-16 13:09:19 +0530

    Add get job by ID feature in API

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -61,3 +61,28 @@ export const getAllJobs=async(req,res)=>{
         
     }
 }
+
+
+export const getJobById =async(req,res)=>{
+    try {
+       const jobId=req.params.id;
+       const job=await Job.findById(jobId);
+     
+       if(!job){
+        return res.status(404).json({
+            message:"Jobs not found",
+            success:true,
+        });
+       }
+
+       return res.status(200).json({
+        job,
+        success:true
+       });
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
