fde1e81a868310129a7707e826436ec51dd8ca3d
NIhal <jaisnj541@gmail.com>
2024-08-24 15:12:21 +0530

    Error Resolved

--- frontend/src/components/ui/avatar.jsx
+++ frontend/src/components/ui/avatar.jsx
@@ -23,7 +23,7 @@ const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      "flex h-full w-full items-center justify-between rounded-full bg-muted",
       className
     )}
     {...props} />
--- frontend/src/components/ui/shared/Navbar.jsx
+++ frontend/src/components/ui/shared/Navbar.jsx
@@ -25,23 +25,22 @@ function Navbar() {
 </Avatar>
 
             </PopoverTrigger>
-            <PopoverContent className='w-8'>
-           <div className='flex gap-4 space-y-2'>
-           <Avatar className="cursor-pointer">
-  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
- 
-  
-</Avatar>
-           </div>
-           <div>
-           <h4 className='font-bold'>Mern Project By Shyam</h4>
-           <p className='text-sm text-muted-foreground'>
-            lorem kj jdj
-           </p>
-           </div>
-              
-            </PopoverContent>
-          </Popover>
+            
+            <PopoverContent className="w-48 p-4">
+      <div className="flex flex-row items-center gap-2">
+       <Avatar className="cursor-pointer">
+       <AvatarImage 
+       src="https://github.com/shadcn.png" alt="@shadcn" />
+    </Avatar>
+    <div className="flex flex-col items-center gap-2" >
+    <h4 className="font-bold text-center">Shyam Babu Jayswal</h4>
+    <p className="text-sm text-muted-foreground text-center"> lorem epsum
+    </p>
+    </div>
+  </div>
+</PopoverContent>
+</Popover>
+
         </div>
       </div>
     </div>
