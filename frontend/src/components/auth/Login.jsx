cc592f66c63f47905397d2290ebfb479d66fc6b8
NIhal <jaisnj541@gmail.com>
2024-08-25 22:31:50 +0530

    SignUp and Login Part done

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
 
+import Navbar from '../shared/Navbar'
+import { Label } from '@radix-ui/react-label'
+import { Input } from '../ui/input'
+import { RadioGroup} from '../ui/radio-group'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
+
 function Login() {
   return (
-    <div>Login</div>
+   
+
+
+  
+    <div>
+        <Navbar/>
+        <div className='flex items-center justify-center max-w-7xl mx-auto'>
+            <form action='' className='w-1/2 border border-green-500 rounded-md p-4 my-10'>
+                <h1 className='font-bold text-xl mb-5'> Login</h1>
+                
+                
+                <div className='my-2'>
+                    <Label>Email</Label>
+                    <Input 
+                        type="email"
+                        placeholder="abcd123@email.com"
+                    />
+                </div>
+                
+                <div className='my-2'>
+                    <Label>Password</Label>
+                    <Input 
+                        type="text"
+                        placeholder="abcde@123456"
+                    />
+                </div> 
+                <div className='flex items-center justify-between'> 
+                 
+                <RadioGroup className='flex items-center gap-4 my-5' >
+      <div className="flex items-center space-x-2">
+          <Input 
+            type="radio"
+            name="role"
+            value="student"
+            className="cursor-pointer"
+
+          />
+        <Label htmlFor="r1">Student</Label>
+      </div>
+      <div className="flex items-center space-x-2">
+      <Input 
+            type="radio"
+            name="role"
+            value="student"
+            className="cursor-pointer"
+
+          /> 
+        <Label htmlFor="r2">Recruiter</Label>
+      </div>
+      
+    </RadioGroup>
+   
+
+                </div>
+                <Button type="submit" className='w-full my-2'  >Login</Button> 
+                <span className="text-sm"> Dont have an account ? <Link to="/signup" className="text-blue-600">
+                    SignUp
+                </Link> </span>
+            </form>
+        </div>
+    </div>
   )
 }
+
+
 
 export default Login
--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -3,6 +3,8 @@ import Navbar from '../shared/Navbar'
 import { Label } from '@radix-ui/react-label'
 import { Input } from '../ui/input'
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 function SignUp() {
   return (
@@ -41,19 +43,43 @@ function SignUp() {
                 </div> 
                 <div className='flex items-center justify-between'> 
                  
-                <RadioGroup className >
+                <RadioGroup className='flex items-center gap-4 my-5' >
       <div className="flex items-center space-x-2">
-        <RadioGroupItem value="default" id="r1" />
+          <Input 
+            type="radio"
+            name="role"
+            value="student"
+            className="cursor-pointer"
+
+          />
         <Label htmlFor="r1">Student</Label>
       </div>
       <div className="flex items-center space-x-2">
-        <RadioGroupItem value="comfortable" id="r2" />
+      <Input 
+            type="radio"
+            name="role"
+            value="student"
+            className="cursor-pointer"
+
+          /> 
         <Label htmlFor="r2">Recruiter</Label>
       </div>
-     
+      
     </RadioGroup>
+    <div className = 'flex items-center gap-2'>
+    <Label>Profile</Label>
+    <Input
+       accept="image/*"
+       type="file"
+       className = "cursor-pointer"  
+    />
+    </div>
 
-                </div> 
+                </div>
+                <Button type="submit" className='w-full my-2'  >SignUp</Button> 
+                <span className='text-sm'>Already have an account ? <Link to='/login' className='text-blue-600'>
+                    Login
+                </Link> </span>
             </form>
         </div>
     </div>
