d4a053346059647d92bd55015ced19e4c6a9c5de
NIhal <jaisnj541@gmail.com>
2024-09-07 21:44:34 +0530

    Make `JobDescription` component dynamic with job-specific data rendering.

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,25 +1,55 @@
 import { Badge } from '../components/ui/badge'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from './ui/button'
+import { useParams } from 'react-router-dom';
+
+import axios from 'axios';
+import { JOB_API_END_POINT } from '@/utils/constant';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSingleJob } from '@/redux/jobSlice';
 
 function JobDescrption() {
   const isApplied = true;
-  
+  const params = useParams();
+  const jobId = params.id; 
+  const {user} =  useSelector(store => store.auth)
+  const {singleJob}  = useSelector(store => store.job)
+
+  const dispatch = useDispatch();
+   
+
+  useEffect(()=>{
+    const fetchSingleJob = async()=>{
+     try {
+      const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, { withCredentials: true });
+      
+
+        if(res.data.success){
+            dispatch(setSingleJob(res.data.job));
+        }
+     } catch (error) {
+        console.log(error)
+     }
+     }
+     fetchSingleJob();
+   } ,[jobId,dispatch,user?._id]);
+
+
   return (
     <div className='max-w-7xl mx-8 my-10'>
      
     <div className='flex items-center justify-between'>
     <div>
-     <h1 className='font-bold text-xl'>Frontend Developer</h1>
+     <h1 className='font-bold text-xl'>{singleJob?.title}</h1>
       <div className='flex items-center gap-2 mt-4'>
             <Badge className={'text-blue-700 font-bold'} variant="ghost">
-             12  Positions
+             {singleJob?.position} Positions
             </Badge>
             <Badge className={'text-[#7209b7] font-bold'} variant="ghost">
-            Part Time
+            {singleJob?.jobType} LPA
             </Badge>
             <Badge className={'text-[#F83002] font-bold'} variant="ghost">
-            24 LPA
+            {singleJob?.salary}
             </Badge>
      </div>
         </div>
@@ -27,13 +57,13 @@ function JobDescrption() {
     </div>
     <h1 className='border-b-2 border-b-gray-300 font-medium py-4'>Job Description</h1>
      <div className='my-4'>
-      <h1 className='font-bold my-1'>Role: <span className='pl-4 font-normal text-gray-800'>Frontend Developer</span></h1>
-      <h1 className='font-bold my-1'>Location: <span className='pl-4 font-normal text-gray-800'>Hyderabad</span></h1>
-      <h1 className='font-bold my-1'>Description: <span className='pl-4 font-normal text-gray-800'>We are hiring a talented Frontend Developer</span></h1>
-      <h1 className='font-bold my-1'>Experience: <span className='pl-4 font-normal text-gray-800'>2 Years</span></h1>
-      <h1 className='font-bold my-1'>Salary: <span className='pl-4 font-normal text-gray-800'>12 LPA</span></h1>
+      <h1 className='font-bold my-1'>Role: <span className='pl-4 font-normal text-gray-800'>{singleJob?.title}</span></h1>
+      <h1 className='font-bold my-1'>Location: <span className='pl-4 font-normal text-gray-800'>{singleJob?.location}</span></h1>
+      <h1 className='font-bold my-1'>Description: <span className='pl-4 font-normal text-gray-800'>{singleJob?.discription}</span></h1>
+      <h1 className='font-bold my-1'>Experience: <span className='pl-4 font-normal text-gray-800'>{singleJob?.experience}</span></h1>
+      <h1 className='font-bold my-1'>Salary: <span className='pl-4 font-normal text-gray-800'>{singleJob?.salary}LPA</span></h1>
       <h1 className='font-bold my-1'>Total Application: <span className='pl-4 font-normal text-gray-800'>Frontend Developer</span></h1>
-      <h1 className='font-bold my-1'>Role: <span className='pl-4 font-normal text-gray-800'>4</span></h1>
+     
       <h1 className='font-bold my-1'>Posted Date: <span className='pl-4 font-normal text-gray-800'>17-07-2024</span></h1>
      </div>
 
